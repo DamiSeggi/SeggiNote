3d8fd705f420ce938c0a4e72586353f8f247453a
DamiSeggi <damiansegginger@gmail.com>
2026-05-22 13:29:46 +0200

    Add pin to NoteCard and css styling

--- Frontend/src/components/NoteCard.jsx
+++ Frontend/src/components/NoteCard.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
+import { managePin } from '../api/noteApi';
 
 function NoteCard({ note }) {
+
+  const[pin, setPin] = useState(false);
+
+    const handlePin = async () => {
+      try {
+        await managePin(note.noteId)
+        setPin(prev => !prev);
+      } catch (err) {
+        setError(err.message)
+      }
+    }
+
   return (
-    <Link to={`/notes/${note.noteId}`} className="note-card">
+  <div className="note-card">
+    <button className="pin-button" onClick={handlePin}>
+      {pin 
+        ? <span style={{ color: "green" }}>pinned</span> 
+        : <span style={{ color: "grey" }}>pin</span>}
+    </button>
+    
+    <Link to={`/notes/${note.noteId}`}>
       <h2>{note.title}</h2>
       <p>{note.content?.substring(0, 100)}...</p>
       <div>{note.tag}</div>
     </Link>
+  </div>
   )
 }
 
